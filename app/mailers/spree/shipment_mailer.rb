ddfba2dbcbfb5c0a29791c167d6d7ce1f5a5d6d4
Daniel Dixon <code@danieldixon.com>
2013-04-29 11:09:36 -0500

    Cherrypicking #55 fix

--- app/mailers/spree/shipment_mailer.rb
+++ app/mailers/spree/shipment_mailer.rb
@@ -4,9 +4,11 @@ class Spree::ShipmentMailer < ActionMailer::Base
   def shipped_email(shipment, resend=false)
     @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
     subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} Shipment Notification ##{shipment.order.number}"
-    mail_params = {:to => shipment.order.email, :subject => subject}
-    mail_params[:from] = shipment.order.store.email if shipment.order.store.email.present?
+    subject += "#{Spree::Config[:site_name]} Shipment Notification ##{@shipment.order.number}"
+    mail_params = {:to => @shipment.order.email, :subject => subject}
+    if @shipment.order.store && @shipment.order.store.email.present?
+      mail_params[:from] = @shipment.order.store.email
+    end
     mail(mail_params)
   end
 end
