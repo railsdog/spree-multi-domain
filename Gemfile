9427b487450868d2a6c91532e02b10dc969312f9
Ryan Bigg <radarlistener@gmail.com>
2013-04-29 11:49:13 +1000

    Remove rails and ffaker dependencies which are just excessive:

--- Gemfile
+++ Gemfile
@@ -1,9 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails'
 gem 'spree_core', :github => "spree/spree", :branch => "1-3-stable"
 gem 'spree_promo', :github => "spree/spree", :branch => "1-3-stable"
 
 gemspec
-
-gem 'ffaker'
--- lib/spree/controller_helpers/common_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::Core::ControllerHelpers::Common.class_eval do
-  def current_currency
-    currency = current_store.try(:default_currency)
-    currency = Spree::Config[:currency] if currency.blank?
-    currency
-  end
-end
-
