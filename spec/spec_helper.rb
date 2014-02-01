a1f20ff09aa8b8afefba84c7a27462d73380221c
Ryan Bigg <radarlistener@gmail.com>
2014-02-01 12:45:49 +0400

    Require pry for debugging help

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 require 'ffaker'
 require 'rspec/rails'
+require 'pry'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
