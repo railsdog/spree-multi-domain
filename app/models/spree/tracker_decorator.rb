e0f8ae7d55bd4866b1b347dc87b1d19d466f01e3
Josua Schmid <josua.schmid@renuo.ch>
2014-11-25 14:46:54 +0100

    reintroduce spree 2.3 compatibility by using an optional argument for the current tracker override

--- app/models/spree/tracker_decorator.rb
+++ app/models/spree/tracker_decorator.rb
@@ -1,7 +1,8 @@
 Spree::Tracker.class_eval do
   belongs_to :store
 
-  def self.current(domain)
+  def self.current(domain = nil)
+    domain ||= Spree::Store.current.url
     Spree::Tracker.where(:active => true, :environment => Rails.env).joins(:store).where("spree_stores.url LIKE ?", "%#{domain}%").first
   end
 end
