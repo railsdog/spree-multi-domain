d585cc824e182765b3cdd21f5e2b4a3f87ce4f63
acreilly <acreilly3@gmail.com>
2015-06-03 11:08:30 +0300

    fixed override virtual path

--- app/overrides/decorate_admin_configurations_index.rb
+++ app/overrides/decorate_admin_configurations_index.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
+Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_configuration",
                      :name => "multi_domain_admin_configurations_menu",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
                      :text => "<%= configurations_sidebar_menu_item Spree.t(:stores_admin), admin_stores_url %>",
