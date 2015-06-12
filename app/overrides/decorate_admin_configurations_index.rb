8a48bee0a6a897a3a8737c0e3732edbceeb95c8d
acreilly <acreilly3@gmail.com>
2015-06-12 10:58:33 -0400

    edited link so won't error with alchemy

--- app/overrides/decorate_admin_configurations_index.rb
+++ app/overrides/decorate_admin_configurations_index.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_configuration",
                      :name => "multi_domain_admin_configurations_menu",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text => "<%= configurations_sidebar_menu_item Spree.t(:stores_admin), admin_stores_url %>",
+                     :text => "<%= configurations_sidebar_menu_item Spree.t(:stores_admin), spree.admin_stores_url %>",
                      :disabled => false)
