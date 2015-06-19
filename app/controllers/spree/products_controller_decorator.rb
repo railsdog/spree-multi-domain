b952dc5303343870eb6cad2f23fd6deeed37d3fb
acreilly <acreilly3@gmail.com>
2015-06-19 11:51:36 -0400

    added index

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,6 +2,12 @@ Spree::ProductsController.class_eval do
 
   before_filter :can_show_product, :only => :show
 
+  def index
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+    @taxonomies = get_taxonomies
+  end
+
   private
 
   def can_show_product
