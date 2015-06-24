738c10d8d391a861e962ae9cd883324e68c45033
acreilly <acreilly3@gmail.com>
2015-06-24 12:54:52 -0400

    refactored method taking out parameters and utilizing Spree::Store.current

--- app/models/spree/shipping_rate_decorator.rb
+++ app/models/spree/shipping_rate_decorator.rb
@@ -1,5 +1,5 @@
 Spree::ShippingRate.class_eval do
-  def self.from_current_store(current_store)
-    joins(shipping_method: :store_shipping_methods).where(spree_store_shipping_methods: {store_id: current_store.id})
+  def self.from_current_store
+    joins(shipping_method: :store_shipping_methods).where(spree_store_shipping_methods: {store_id: Spree::Store.current.id})
   end
 end
