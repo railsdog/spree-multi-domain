6f10f3d26e2536043ac8fb366093553a2407aa63
acreilly <acreilly3@gmail.com>
2015-06-24 12:54:29 -0400

    fixed Spree::Store.current method & fixed payments to only show payments for that store

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -3,10 +3,8 @@ Spree::PaymentMethod.class_eval do
   has_many :stores, :through => :store_payment_methods
 
   def self.available(display_on = 'both', store = nil)
-    result = all.select do |p|
-      p.active &&
-        (store.nil? || store.payment_methods.empty? || store.payment_methods.include?(p)) &&
-        (p.display_on == display_on.to_s || p.display_on.blank?)
+    result = Spree::Store.current.payment_methods.select do |p|
+      p.active && (p.display_on == display_on.to_s || p.display_on.blank?)
     end
   end
 end
